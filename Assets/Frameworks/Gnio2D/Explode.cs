b0ad40978790ae345c65d3e4af3242ad6f436c38
gnios <eugenio00@gmail.com>
2016-07-29 01:04:12 -0300

    Finalizei o comportamento dos projeteis

--- Assets/Frameworks/Gnio2D/Explode.cs
+++ Assets/Frameworks/Gnio2D/Explode.cs
@@ -5,21 +5,30 @@ namespace Assets.Frameworks.Gnio2D
     public class Explode : MonoBehaviour
     {
         public GameObject Projectile;
-        private GameObject _clone;
+
+        private GameObject projectileRight;
+        private GameObject projectileLeft;
+        private GameObject projectileUp;
+        private GameObject projectileDown;
 
         private void OnMouseDown()
         {
-            _clone = (GameObject)Instantiate(Projectile, gameObject.transform.position, gameObject.transform.rotation);
-            _clone.transform.position = gameObject.transform.position;
+            projectileRight = (GameObject)Instantiate(Projectile, gameObject.transform.position, Quaternion.AngleAxis(0, Vector3.forward));
+            projectileLeft = (GameObject)Instantiate(Projectile, gameObject.transform.position, Quaternion.AngleAxis(180, Vector3.forward));
+            projectileUp = (GameObject)Instantiate(Projectile, gameObject.transform.position, Quaternion.AngleAxis(90, Vector3.forward));
+            projectileDown = (GameObject)Instantiate(Projectile, gameObject.transform.position, Quaternion.AngleAxis(270f, Vector3.forward));
 
-            // Add force to the cloned object in the object's forward direction
             Debug.Log("Sprite Clicked");
         }
 
         private void Update()
         {
-            if (_clone != null)
-                _clone.GetComponent<Rigidbody>().AddForce(_clone.transform.right * 20f);
+            if (projectileRight == null)
+                return;
+            projectileRight.GetComponent<Rigidbody>().AddRelativeForce(projectileRight.transform.right * 20f);
+            projectileLeft.GetComponent<Rigidbody>().AddRelativeForce((projectileLeft.transform.right * -1) * 20f);
+            projectileUp.GetComponent<Rigidbody>().AddRelativeForce((projectileUp.transform.up * -1) * 20f);
+            projectileDown.GetComponent<Rigidbody>().AddRelativeForce((projectileDown.transform.up) * 20f);
         }
     }
 }
